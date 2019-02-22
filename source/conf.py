9d74df5ded34c031958854aa1e4a9a92701b3222
ElliottM-on-Git <47116301+ElliottM-on-Git@users.noreply.github.com>
2019-02-21 19:46:55 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -6,9 +6,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Sample Resume with Sphinx'
-copyright = u'2019, Mark Hoeber'
-author = u'Mark Hoeber'
+project = u'A Learning Exercise'
+copyright = u'2019, Elliott M.'
+author = u'Elliott M.'
 
 
 #version = u'1.0'
